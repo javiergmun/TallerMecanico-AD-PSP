f7415e5cbcf09438cd0ad69d0e3eaa68641bb058
Adrian <adrian.loz@hotmail.com>
2022-02-04 11:23:01 +0100

    Empezando con el Trabajo

---
+++ taller/src/test/java/com/taller2dam/taller/TallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller2dam.taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
