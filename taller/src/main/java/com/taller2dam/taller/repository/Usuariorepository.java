68dfd715385db876937ca4e45d593ecd7b7692d4
Adrian <adrian.loz@hotmail.com>
2022-02-17 09:10:08 +0100

    test

--- taller/src/main/java/com/taller2dam/taller/repository/Usuariorepository.java
+++ taller/src/main/java/com/taller2dam/taller/repository/Usuariorepository.java
@@ -3,5 +3,8 @@ package com.taller2dam.taller.repository;
 import com.taller2dam.taller.dao.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface Usuariorepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByName(String name);
 }
