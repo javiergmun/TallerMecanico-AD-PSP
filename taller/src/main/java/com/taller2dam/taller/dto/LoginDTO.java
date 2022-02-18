e33c9a594fbd7a8c98a6dbde8823c14530458800
javiergmun <javiergmun@gmail.com>
2022-02-18 10:40:18 +0100

    Dto and Mapper

--- taller/src/main/java/com/taller2dam/taller/dto/LoginDTO.java
+++ taller/src/main/java/com/taller2dam/taller/dto/LoginDTO.java
@@ -1,4 +1,27 @@
 package com.taller2dam.taller.dto;
 
+import com.taller2dam.taller.dao.Servicio;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDTO {
+
+    private Long id;
+
+    @Email(regexp = ".*@.*\\..*", message = "Email debe ser correcto")
+    private String correo;
+
+    @NotBlank(message = "Debes incluir una contraseña")
+    private String password;
+
+    private String token;
+
 }
--- taller/src/main/java/com/taller2dam/taller/dto/ServicioDTO.java
+++ taller/src/main/java/com/taller2dam/taller/dto/ServicioDTO.java
@@ -1,4 +1,31 @@
 package com.taller2dam.taller.dto;
 
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServicioDTO {
+    private Long id;
+
+    @Min(message = "El precio no puede ser negativo", value =0)
+    private Double precio;
+
+    @NotBlank(message = "Debes incluir un tipo")
+    private String tipo;
+
+    @NotBlank(message = "Debes fijar un tiempo de trabajo")
+    private Double tiempo;
+
+    @NotBlank(message = "Debes incluir una fecha de inicio")
+    private LocalDateTime fecha_inicio;
+
+    private LocalDateTime fecha_fin;
 }
