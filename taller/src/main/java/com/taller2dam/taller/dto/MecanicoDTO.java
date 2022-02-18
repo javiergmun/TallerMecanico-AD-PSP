05d89a82ab255a4181ebdad820f2418097732827
javiergmun <javiergmun@gmail.com>
2022-02-18 09:53:30 +0100

    Actualizar pom

--- taller/src/main/java/com/taller2dam/taller/dto/MecanicoDTO.java
+++ taller/src/main/java/com/taller2dam/taller/dto/MecanicoDTO.java
@@ -1,4 +1,18 @@
 package com.taller2dam.taller.dto;
 
+import lombok.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MecanicoDTO {
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    private String nombre;
+
 }
