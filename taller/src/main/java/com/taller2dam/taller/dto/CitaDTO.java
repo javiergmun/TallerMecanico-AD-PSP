a67d2cc43b3d9b9326ed421ae721161b0c8681c7
Andrea <andrea.gomez@alumno.iesluisvives.org>
2022-02-18 10:42:02 +0100

    Se completan los DTO de vehículo, usuario y cita

---
+++ taller/src/main/java/com/taller2dam/taller/dto/CitaDTO.java
@@ -0,0 +1,32 @@
+package com.taller2dam.taller.dto;
+
+import com.taller2dam.taller.dao.Mecanico;
+import com.taller2dam.taller.dao.Servicio;
+import com.taller2dam.taller.dao.Usuario;
+import lombok.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CitaDTO {
+
+
+    private long id;
+    @Min(message = "El precio no puede ser negativo", value = 0)
+    private Double precio;
+    @NotBlank(message = "Debes introducir una fecha")
+    private LocalDateTime fecha;
+    @NotBlank(message = "Debes introducir un usuario")
+    private Usuario usuario;
+    @NotBlank(message = "Debes introducir un mecánico")
+    private Mecanico mecanico;
+    @NotBlank(message = "Debes introducir un servicio")
+    private Servicio servicio;
+
+}
--- taller/src/main/java/com/taller2dam/taller/dto/UsuarioDTO.java
+++ taller/src/main/java/com/taller2dam/taller/dto/UsuarioDTO.java
@@ -1,4 +1,32 @@
 package com.taller2dam.taller.dto;
 
+import com.taller2dam.taller.dao.Direccion;
+import com.taller2dam.taller.dao.Vehiculo;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
+
+    private long id;
+    @NotBlank(message = "Debes introducir un dni")
+    private String dni;
+    @NotBlank(message = "Debes introducir un nombre")
+    private String nombre;
+    @NotBlank(message = "Debes indicar si es un administrador")
+    private Boolean administrador;
+    @NotBlank(message = "Debes introducir un telefono")
+    private Integer telefono; //valorar cambiar a String. No necesitamos un Integer si no vamos a hacer cálculos con él
+    @NotBlank(message = "Debes introducir una dirección")
+    private Direccion direccion;
+    //Esta lista si puede estar vacía. Un usuario puede no tener aun ningun vahículo.
+    //Se hará el registro del vehículo en el layout correspondiente.
+    private Set<Vehiculo> vehiculos;
+
 }
