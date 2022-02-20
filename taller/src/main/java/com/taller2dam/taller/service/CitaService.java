01b2a54009059855c266e56042fe0dc40f652db0
Andrea <andrea.gomez@alumno.iesluisvives.org>
2022-02-20 15:36:10 +0100

    Se completan las clases Service de Vehiculo, Usuario y Cita

--- taller/src/main/java/com/taller2dam/taller/service/CitaService.java
+++ taller/src/main/java/com/taller2dam/taller/service/CitaService.java
@@ -1,4 +1,52 @@
 package com.taller2dam.taller.service;
 
+import com.taller2dam.taller.dao.Cita;
+import com.taller2dam.taller.repository.CitaRepository;
+import com.taller2dam.taller.repository.CitaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class CitaService {
+    CitaRepository citaRepository;
+
+
+    //CONSULTAS CRUD BÁSICAS
+
+    public List<Cita> findAllCitas() {
+        return citaRepository.findAll();
+    }
+
+    public Optional<Cita> findCitaById(Long citaId) {
+        return citaRepository.findById(citaId);
+    }
+
+    public Cita saveCita(Cita cita) {
+        return citaRepository.save(cita);
+    }
+
+    //A falta de probar su funcionamiento
+    public Optional<Cita> updateCita(Cita cita, Cita citaNueva) {
+        Optional<Cita>cita1= findCitaById(cita.getId());
+        cita1.ifPresent(c -> {
+
+            c.setId(citaNueva.getId());
+            c.setPrecio(citaNueva.getPrecio());
+            c.setFecha(citaNueva.getFecha());
+            c.setUsuario(citaNueva.getUsuario());
+            c.setMecanico(citaNueva.getMecanico());
+            c.setServicio(citaNueva.getServicio());
+            citaRepository.save(c);
+
+        });
+        return cita1;
+
+    }
+
+    public void deleteCita(Cita cita) {
+        citaRepository.delete(cita);
+    }
+
+
+
 }
--- taller/src/main/java/com/taller2dam/taller/service/UsuarioService.java
+++ taller/src/main/java/com/taller2dam/taller/service/UsuarioService.java
@@ -1,4 +1,56 @@
 package com.taller2dam.taller.service;
 
+import com.taller2dam.taller.dao.Usuario;
+import com.taller2dam.taller.repository.UsuarioRepository;
+import com.taller2dam.taller.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class UsuarioService {
+    UsuarioRepository usuarioRepository;
+
+
+    //CONSULTAS CRUD BÁSICAS
+
+    public List<Usuario> findAllUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> findUsuarioById(Long usuarioId) {
+        return usuarioRepository.findById(usuarioId);
+    }
+
+    public Usuario saveUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    //A falta de probar su funcionamiento
+    public Optional<Usuario> updateUsuario(Usuario usuario, Usuario usuarioNuevo) {
+        Optional<Usuario>usuario1= findUsuarioById(usuario.getId());
+        usuario1.ifPresent(u -> {
+
+            u.setDni(usuarioNuevo.getDni());
+            u.setId(usuarioNuevo.getId());
+            u.setNombre(usuarioNuevo.getNombre());
+            u.setAdministrador(usuarioNuevo.getAdministrador());
+            u.setTelefono(usuarioNuevo.getTelefono());
+            u.setDireccion(usuarioNuevo.getDireccion());
+            u.setCorreo(usuarioNuevo.getCorreo());
+            u.setPassword(usuarioNuevo.getPassword());
+            u.setVehiculos(usuarioNuevo.getVehiculos());
+            u.setLogin(usuarioNuevo.getLogin());
+
+            usuarioRepository.save(u);
+
+        });
+        return usuario1;
+
+    }
+
+    public void deleteUsuario(Usuario usuario) {
+        usuarioRepository.delete(usuario);
+    }
+
 }
+
