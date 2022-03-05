3877520367ab5b3f9a88849c9a94df9a3fb0160d
MarioGG <ggperpi@gmail.com>
2022-03-05 19:45:58 +0100

    Cambio de Direccion en seguridad

--- taller/src/main/java/com/taller2dam/taller/security/jwt/model/JwtUserResponse.java
+++ taller/src/main/java/com/taller2dam/taller/security/jwt/model/JwtUserResponse.java
@@ -1,6 +1,5 @@
 package com.taller2dam.taller.security.jwt.model;
 
-import com.taller2dam.taller.dao.Direccion;
 import com.taller2dam.taller.dao.Vehiculo;
 import com.taller2dam.taller.dto.UsuarioDTO;
 import lombok.Builder;
@@ -18,7 +17,7 @@ public class JwtUserResponse extends UsuarioDTO {
     private String token;
 
     @Builder(builderMethodName = "jwtUserResponseBuilder")
-    public JwtUserResponse(long id, @NotBlank(message = "Debes introducir un dni") String dni, @NotBlank(message = "Debes introducir un nombre") String username, @NotBlank(message = "Debes introducir un telefono") String telefono, @NotBlank(message = "Debes introducir un correo") String correo, @NotBlank(message = "Debes introducir una dirección") Direccion direccion, Set<Vehiculo> vehiculos, @NotBlank(message = "Debes introducir una pass") String password, String imagen, String bitmap, Set<String> roles, String token) {
+    public JwtUserResponse(long id, @NotBlank(message = "Debes introducir un dni") String dni, @NotBlank(message = "Debes introducir un nombre") String username, @NotBlank(message = "Debes introducir un telefono") String telefono, @NotBlank(message = "Debes introducir un correo") String correo, @NotBlank(message = "Debes introducir una dirección") String direccion, Set<Vehiculo> vehiculos, @NotBlank(message = "Debes introducir una pass") String password, String imagen, String bitmap, Set<String> roles, String token) {
         super(id, dni, username, telefono, correo, direccion, vehiculos, password, imagen, bitmap, roles);
         this.token = token;
     }
