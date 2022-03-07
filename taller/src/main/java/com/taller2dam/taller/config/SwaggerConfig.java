7c45f2f165a06aaf92bcfddaae4cf783b327ea9c
MarioGG <ggperpi@gmail.com>
2022-03-07 21:45:53 +0100

    swagger operativo

--- taller/src/main/java/com/taller2dam/taller/config/SwaggerConfig.java
+++ taller/src/main/java/com/taller2dam/taller/config/SwaggerConfig.java
@@ -2,10 +2,6 @@ package com.taller2dam.taller.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.hateoas.client.LinkDiscoverer;
-import org.springframework.hateoas.client.LinkDiscoverers;
-import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
-import org.springframework.plugin.core.SimplePluginRegistry;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,13 +9,13 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+
 
 @Configuration
-//@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
 
@@ -47,3 +43,5 @@ public class SwaggerConfig {
     }
 
 }
+
+
